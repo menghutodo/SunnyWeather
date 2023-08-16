b8f69fa8468e1dab165cdd239732c67214367b14
menghu <menghu98621@163.com>
2023-08-16 14:54:11 +0800

    修改-让背景图和状态栏融合到一起。

--- app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
@@ -1,5 +1,6 @@
 package com.sunnyweather.android.ui.weather
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -24,9 +25,12 @@ class WeatherActivity : AppCompatActivity() {
 
     private val binding : ActivityWeatherBinding get() = _binding!!
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val decorView = window.decorView
+        decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        window.statusBarColor = Color.TRANSPARENT
+
         _binding = ActivityWeatherBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
